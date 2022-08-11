e145ffb87e8b2d82dfa2e6298775af6ee9edc512
Alexander Simakov <a.simakov.hh@gmail.com>
2022-08-11 15:47:03 +0200

    ex2

---
+++ exercise (1).py
@@ -0,0 +1,80 @@
+# do exercise here
+# upload to github for portfolio
+# thank me later when your git graph is as green as the python money u gonna earn
+"""1"""
+"""
+a = 2 + 2
+print(a)
+
+
+a = 23 + 2
+print(a)
+
+a = 45 - 23
+print(a)
+
+a = 346 - 34
+print(a)
+
+a = 2 * 33
+print(a)
+
+a = 344 * 2
+print(a)
+
+a = 4 / 78
+print(a)
+
+a = 5 / 2
+print(a)
+
+a = 4 % 2
+print(a)
+
+a = 3434 % 3
+print(a)
+
+
+a = 3434 ** 3
+print(a)
+
+
+a = 3434 ** 44
+print(a)
+
+
+a = 3434  // 3
+print(a)
+
+
+a = 3434 // 9
+print(a)
+"""
+
+
+"""2"""
+
+"""
+print(max(2, 5 ,6))
+print(max(23,567,567))
+
+print()
+print(min(2, 5 ,6))
+print(min(23,567,567))
+
+print(abs(-4534.345))
+print(abs(-234.33))
+
+print(pow(23,33))
+print(pow(324,55))
+"""
+import math
+print(math.ceil(324.32)) #comment
+
+print(math.ceil(7/5))
+print(math.floor(32874.3432))
+print(math.floor(7534/345))
+
+print(max(True, False))
+
+print(abs(53-34j))
